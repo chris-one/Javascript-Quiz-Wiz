eeb4102b22d5eabb05b879b3457cc4642eb68fe4
chris-one <102882070+chris-one@users.noreply.github.com>
2022-06-12 15:16:30 -0700

    started working on javascript

--- assests/app.js
+++ assests/app.js
@@ -1,33 +1,60 @@
-var allQuestions = [ 
-    'What is a String?', 
-    'What is the definition of a Number?',
-    'What is a Boolean?',
-    'What does Null define?',
-    'What is an Array?',
-    'What is the purpose of a function?',
-]
+// var allQuestions = [ 
+//     'What is a String?', 
+//     'What is the definition of a Number?',
+//     'What is a Boolean?',
+//     'What does Null define?',
+//     'What is an Array?',
+//     'What is the purpose of a function?',
+// ]
+
+var startBtn = document.getElementById('beginButton');
+
+var thePlayEl = document.querySelector('.container');
+
 
 var timerEl = document.getElementById('myTimer');
 
-
-
-
-
 function startGame() {
-    
+    thePlayEl.style.display = 'block';
+    startBtn.style.display = 'none';
+    setTime()
+    nextQuestion()
 }
 
 function nextQuestion() {
-
+    var random = Math.floor(Math.random() * ansQuest.length);
+    let question = ansQuest
+    console.log(question);
 }
 
-var inputWrong = document.getElementById('mybutton');
-inputWrong.addEventListener('click', wrongAnswer); 
+var ansQuest = [
+    {
+        question: "What is a String?",
+        answer: {
+            a: "A string is a sequence of one or more characters that may consist of letters, numbers, or symbols.",
+            b: "Is what you tie your shoes with, so you don't trip over yourself in public.",
+            c: "Strings are used to make clothing to wear, to cover yourself everyday.",
+        },
+        correctAnswer: 'a',
+    },
+    {
+        question: "What is the a definiton of a Number?",
+        answer: {
+            a: "Is something we learned in elemantary to count to 10",
+            b: "Are the wrong 7 digit's the girl gave you, that doesn't like you.",
+            c: "The number data type is used to represent positive or negative number with or without decimal place.",
+        },
+        correctAnswer: 'c',
+    }
+]
 
-function wrongAnswer() {
-    document.getElementById('mybutton').textContent = 'Try again'
-    setTime()
-}
+// var inputWrong = document.getElementById('mybutton');
+// inputWrong.addEventListener('click', wrongAnswer); 
+
+// function wrongAnswer() {
+    // document.getElementById('mybutton').textContent = 'Try again'
+    // setTime()
+// }
 
 // timer
 function setTime() {
@@ -54,3 +81,5 @@ function setTime() {
       }
     }, 1000);
   }
+
+  startBtn.addEventListener('click', startGame);
